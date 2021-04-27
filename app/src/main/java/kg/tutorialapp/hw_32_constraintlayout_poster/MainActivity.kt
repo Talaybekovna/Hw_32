20fcf9d0535fe0da5478cab119c3b95744423590
Kunduz <talaybekovna.kunduz@gmail.com>
2021-04-27 22:45:30 +0600

    added two movie posters

---
+++ app/src/main/java/kg/tutorialapp/hw_32_constraintlayout_poster/MainActivity.kt
@@ -0,0 +1,11 @@
+package kg.tutorialapp.hw_32_constraintlayout_poster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/kg/tutorialapp/hw_32_constraintlayout_poster/MoviePoster_2.kt
@@ -0,0 +1,11 @@
+package kg.tutorialapp.hw_32_constraintlayout_poster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoviePoster_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_poster_2)
+    }
+}
